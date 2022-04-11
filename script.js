77679028e910ec3a4a34f351d7e050caea43e952
TheLimaVinicius <vinny-link@live.com>
2022-04-11 18:31:48 -0300

    Aprimorei o botao Chutar e deixei algumas emplementacoes no gatilho pro proximo commit

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-var numeroSecreto = parseInt(Math.random() *11);
+const numeroSecreto = parseInt(Math.random() * 11);
+const valorInserido = document.getElementById("valor");
+const buttonKick = document.getElementById("btnKick");
+const buttonTryAgain = document.getElementById("btnTryAgain");
+
+buttonKick.disabled = true;
+valorInserido.addEventListener("input", function (event) {
+  let conteudo = valorInserido.value;
+
+  if (conteudo !== NaN && conteudo !== "") {
+    buttonKick.disabled = false;
+  } else {
+    buttonKick.disabled = true;
+  }
+});
 
 function Chutar() {
-    var elementoResultado = document.getElementById("resultado");
-    var chute = parseInt(document.getElementById("valor").value);
+    let elementoResultado = document.getElementById("resultado");
+    let chute = parseInt(document.getElementById("valor").value);
     if (chute == numeroSecreto) {
         elementoResultado.innerHTML = "Você acertou!"
-    } else if (chute > 10 || chote < 0) {
+    } else if (chute > 10 || chute < 0) {
         elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
     } else {
         elementoResultado.innerHTML = "Errou! O número secreto era: " + numeroSecreto;
